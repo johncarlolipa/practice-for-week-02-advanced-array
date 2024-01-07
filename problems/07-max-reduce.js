5958e42f9b80a19b4e2a18603469125de806f443
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-07 11:09:54 +0800

    done exercise 8

--- problems/07-max-reduce.js
+++ problems/07-max-reduce.js
@@ -16,6 +16,13 @@ console.log(maxWithReduce([-2, -3, -7, 3])); // 3
 
 let maxWithReduce = function(nums) {
     // Your code here
+    return nums.reduce((max, num) => {
+      if(num > max){
+        return num
+      } else {
+        return max
+      }
+    })
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
