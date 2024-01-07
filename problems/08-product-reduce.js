af0bda37150f815de79883500c0d1a7a3e02f3a2
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-07 11:11:05 +0800

    done exercise 9

--- problems/08-product-reduce.js
+++ problems/08-product-reduce.js
@@ -17,6 +17,9 @@ console.log(productWithReduce([4, 3])); // 12
 
 let productWithReduce = function(nums) {
     // Your code here
+    return nums.reduce((max, num) => {
+      return max * num
+    })
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -25,4 +28,4 @@ try {
   module.exports = productWithReduce;
 } catch (e) {
   module.exports = null;
-}
+}
