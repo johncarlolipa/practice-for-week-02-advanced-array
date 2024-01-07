59afbae44d35b17f2852c1a3a4d958642f27d8ff
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-07 15:09:01 +0800

    done exercise 16

--- problems/15-choose-primes.js
+++ problems/15-choose-primes.js
@@ -20,10 +20,23 @@ console.log(choosePrimes([5, 6, 4, 11, 2017])); // [ 5, 11, 2017 ]
 
 let isPrime = function(num) {
     // Your code here
+    if(num < 2){
+        return false
+    }
+
+    for (let i = 2; i <= Math.sqrt(num); i++){
+        if(num % i === 0){
+            return false
+        }
+    }
+
+    return true
 }
+
 
 let choosePrimes = function(nums) {
     // Your code here
+    return nums.filter(num => isPrime(num))
 }
 
 // alternative solution using Array.filter
@@ -37,4 +50,4 @@ try {
     module.exports = choosePrimes;
 } catch (e) {
     module.exports = null;
-}
+}
