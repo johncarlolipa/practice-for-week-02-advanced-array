1ff886b20fc2c46f03363d320b4c6779eea5ed69
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-07 10:02:53 +0800

    done exercise 1

--- problems/00-sum-forEach.js
+++ problems/00-sum-forEach.js
@@ -12,8 +12,17 @@ console.log(sumForEach([])); // 0
 */
 
 let sumForEach = function(arr) {
-  // Your code here
+  let total = 0
+
+  arr.forEach((num) => {
+    return total += num
+  })
+  return total
 };
+
+console.log(sumForEach([4, 3, -1, 10])); // 16
+console.log(sumForEach([6, 7, 2])); // 15
+console.log(sumForEach([])); // 0
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
