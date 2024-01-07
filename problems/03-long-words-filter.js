e6bc7b2ea9ae43eea38febd201b07cf53e4e08a5
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-07 10:27:08 +0800

    done exercise 4

--- problems/03-long-words-filter.js
+++ problems/03-long-words-filter.js
@@ -15,6 +15,11 @@ console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // 
 
 let longWords = function(words) {
     // Your code here
+    return words.filter((word) => {
+      if (word.length > 5){
+        return word
+      }
+    })
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -23,4 +28,4 @@ try {
   module.exports = longWords;
 } catch (e) {
   module.exports = null;
-}
+}
